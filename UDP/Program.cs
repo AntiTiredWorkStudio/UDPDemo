55e1e16d9859cf87914332d5ffddefe52f1c8a79
konglf2112 <konglf2112@foxmail.com>
2019-11-02 22:24:13 +0800

    修改结构
    Signed-off-by: konglf2112 <konglf2112@foxmail.com>

--- UDP/Program.cs
+++ UDP/Program.cs
@@ -9,126 +9,39 @@ using System.Net;
 using Newtonsoft;
 namespace UDP
 {
-    public class DataStruct
+    class UDPScene : UDPHandle<string>
     {
-        public string id;
-        public string msg;
+        UDPManager<string> chatManager;
+        public UDPScene()
+        {
+            chatManager = new UDPManager<string>(this) ;
+            Chat();
+        }
+
+        void Chat()
+        {
+            while (true)
+            {
+                chatManager.AddMsg(Console.ReadLine());
+            }
+        }
+
+        public void DataTransport(DataTransfer<string> data)
+        {
+            Console.WriteLine(data.id + ":" + data.data);
+        }
+
+        public void Log(string msg)
+        {
+            Console.WriteLine(msg);
+        }
     }
     
     class Program
     {
-        static string id = "nan";
-        static string ID
-        {
-            get
-            {
-                if (id == "nan") { Random rand = new Random(); id = "user_"+(1000000+rand.Next()%999999); }
-                return id;
-            }
-        }
-        
         static void Main(string[] args)
         {
-            Console.WriteLine("用户:" + ID);
-            Console.Read();
-            Console.Clear();
-
-            Thread sendThread = null;
-            Thread reciveThread = null;
-            //Newtonsoft.Json.JsonConvert.SerializeObject(
-            reciveThread = new Thread(ReciveFunc); reciveThread.IsBackground = false;
-            reciveThread.Start();
-            sendThread = new Thread(SendFunc); sendThread.IsBackground = false;
-            sendThread.Start();
-        }
-        static void ReciveFunc()
-        {
-            int port = 8001;
-            string hostName = Dns.GetHostName();
-            IPHostEntry localhost = Dns.GetHostEntry(hostName);
-            List<IPAddress> remoteIp = (
-                new List<IPAddress>(
-                    from IPAddress tip in localhost.AddressList 
-                    where tip.AddressFamily == AddressFamily.InterNetwork 
-                    select tip)
-                 );
-            int seek = 0;
-            IPEndPoint local = new IPEndPoint(remoteIp[seek], port);
-            UdpClient RecviceClient = null;
-            while (true)
-            {
-                try
-                {
-                    RecviceClient = new UdpClient(local);
-                    Console.WriteLine("接收线程:"+local.ToString()+" 启动成功");
-                    break;
-                }
-                catch
-                {
-                    Console.WriteLine("端口占用:" + local.ToString() + " 重置端口");
-                    if (remoteIp.Count > 1)
-                    {
-                        local = new IPEndPoint(remoteIp[++seek], port);
-                    } else { local.Port++; }
-                    continue;
-                }
-            }
-            IPEndPoint remote = new IPEndPoint(IPAddress.Any, 8001);
-            //IPEndPoint remote01 = new IPEndPoint(IPAddress.Any, 8002);
-            while (true)
-            {
-                try
-                {
-                    byte[] recivcedata = RecviceClient.Receive(ref remote); //RecviceClient.Receive(ref remote01);
-                    string strMsg = System.Text.Encoding.UTF8.GetString(recivcedata, 0, recivcedata.Length);
-                    DataStruct data = Newtonsoft.Json.JsonConvert.DeserializeObject<DataStruct>(strMsg);
-                    if(data.id != ID)
-                        Console.WriteLine(data.id+":"+data.msg);
-                }
-                catch
-                {
-                    break;
-                }
-            }
-        }
-        static void SendFunc()
-        {
-            int port = 8001;
-            UdpClient SendClient = null;
-            while (true)
-            {
-                try
-                {
-                    SendClient = new UdpClient(new IPEndPoint(IPAddress.Any, 0));
-                    break;
-                }
-                catch
-                {
-
-                    continue;
-                }
-            }
-            IPAddress remoteIp = IPAddress.Parse("255.255.255.255");
-            IPEndPoint ip = new IPEndPoint(remoteIp, port);
-            Console.WriteLine("发送线程启动成功:"+ip.ToString());
-            
-            while (true)
-            {
-                try
-                {
-                    string msg = Console.ReadLine();
-                    if(msg == "")continue;
-                    DataStruct data = new DataStruct();
-                    data.id = ID; data.msg = msg;
-                    byte[] msgBuff = System.Text.Encoding.UTF8.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(data));
-                    SendClient.EnableBroadcast = true;//.Send(msgBuff, msgBuff.Length);
-                    SendClient.Send(msgBuff, msgBuff.Length, ip);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("发送失败:" + ex.ToString());
-                }
-            }
+            new UDPScene();
         }
     }
 }
---
+++ UDP/UDPManager.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Net;
+using Newtonsoft;
+
+namespace UDP
+{
+    public interface UDPHandle<T>
+    {
+        void Log(string msg);
+        void DataTransport(DataTransfer<T> data);
+    }
+
+    public class DataTransfer<T>
+    {
+        public string id;
+        public T data;
+    }
+    
+
+    class UDPManager<T>
+    {
+        public UDPManager(UDPHandle<T> handle)
+        {
+            InitManager(handle);
+        }
+        UDPHandle<T> handleObject;
+        Queue<T> TargetMsg;
+        public void AddMsg(T msg)
+        {
+            if (TargetMsg != null) TargetMsg.Enqueue(msg);
+        }
+        void InitManager(UDPHandle<T> handle)
+        {
+            handleObject = handle;
+            handleObject.Log("用户:" + ID);
+            TargetMsg = new Queue<T>();
+            Thread sendThread = null;
+            Thread reciveThread = null;
+            reciveThread = new Thread(ReciveFunc);
+            reciveThread.IsBackground = false;
+            reciveThread.Start();
+            sendThread = new Thread(SendFunc);
+            sendThread.IsBackground = false;
+            sendThread.Start();
+        }
+        
+        string id = "nan";
+        public string ID
+        {
+            get
+            {
+                if (id == "nan") { Random rand = new Random(); id = "user_" + (1000000 + rand.Next() % 999999); }
+                return id;
+            }
+        }
+
+
+        void ReciveFunc()
+        {
+            int port = 8001;
+            string hostName = Dns.GetHostName();
+            IPHostEntry localhost = Dns.GetHostEntry(hostName);
+            List<IPAddress> remoteIp = (
+                new List<IPAddress>(
+                    from IPAddress tip in localhost.AddressList
+                    where tip.AddressFamily == AddressFamily.InterNetwork
+                    select tip)
+                 );
+            int seek = 0;
+            IPEndPoint local = new IPEndPoint(remoteIp[seek], port);
+            UdpClient RecviceClient = null;
+            while (true)
+            {
+                try
+                {
+                    RecviceClient = new UdpClient(local);
+                    handleObject.Log("接收线程:" + local.ToString() + " 启动成功");
+                    break;
+                }
+                catch
+                {
+                    handleObject.Log("端口占用:" + local.ToString() + " 重置端口");
+                    if (remoteIp.Count > 1)
+                    {
+                        local = new IPEndPoint(remoteIp[++seek], port);
+                    }
+                    else { local.Port++; }
+                    continue;
+                }
+            }
+            IPEndPoint remote = new IPEndPoint(IPAddress.Any, 8001);
+            //IPEndPoint remote01 = new IPEndPoint(IPAddress.Any, 8002);
+            while (true)
+            {
+                try
+                {
+                    byte[] recivcedata = RecviceClient.Receive(ref remote); //RecviceClient.Receive(ref remote01);
+                    string strMsg = System.Text.Encoding.UTF8.GetString(recivcedata, 0, recivcedata.Length);
+                    DataTransfer<T> data = Newtonsoft.Json.JsonConvert.DeserializeObject<DataTransfer<T>>(strMsg);
+                    if (data.id != ID) handleObject.DataTransport(data);
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+
+
+        void SendFunc()
+        {
+            int port = 8001;
+            UdpClient SendClient = null;
+            while (true)
+            {
+                try
+                {
+                    SendClient = new UdpClient(new IPEndPoint(IPAddress.Any, 0));
+                    break;
+                }
+                catch
+                {
+
+                    continue;
+                }
+            }
+            IPAddress remoteIp = IPAddress.Parse("255.255.255.255");
+            IPEndPoint ip = new IPEndPoint(remoteIp, port);
+            handleObject.Log("发送线程启动成功:" + ip.ToString());
+            while (true)
+            {
+                if (TargetMsg.Count == 0)
+                {
+                    Thread.Sleep(500);
+                    continue;
+                }
+                try
+                {
+                    T content = TargetMsg.Dequeue();
+                    if (content == null) continue;
+                    DataTransfer<T> data = new DataTransfer<T>();
+
+                    data.id = ID; data.data = content;
+                    byte[] msgBuff = System.Text.Encoding.UTF8.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(data));
+                    SendClient.EnableBroadcast = true;
+                    SendClient.Send(msgBuff, msgBuff.Length, ip);
+                }
+                catch (Exception ex)
+                {
+                    handleObject.Log("发送失败:" + ex.ToString());
+                }
+            }
+        }
+    }
+}
